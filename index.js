accbdc1a19b2a94c95e39513ae6f4601b1a33556
AniketParmarRajput <rajputaniket092@gmail.com>
2025-11-09 14:35:38 +0530

    third

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import express from 'express';
 const app =express();
 const port=3000;
 app.get('/',(req,res)=>{
-    res.send("eneter the value")
-})
-app.get('/',(req,res)=>{
-    res.send("eneter the ")
+    res.send("eneter the")
 })
 app.listen(port,()=>{
 
