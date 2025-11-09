eed03fe1195643b138ff4a81816cd67f3d711f6a
AniketParmarRajput <rajputaniket092@gmail.com>
2025-11-09 14:31:23 +0530

    second chnage

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port=3000;
 app.get('/',(req,res)=>{
     res.send("eneter the value")
 })
+app.get('/',(req,res)=>{
+    res.send("eneter the ")
+})
 app.listen(port,()=>{
 
     console.log("server start")
