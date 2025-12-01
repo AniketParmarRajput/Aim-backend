11929d832ef9435316112eb58dc56c6addb9ba44
AniketParmarRajput <rajputaniket092@gmail.com>
2025-12-01 15:26:30 +0530

    create a jwt token but not work properly

--- src/Controllers/Login.controller.js
+++ src/Controllers/Login.controller.js
@@ -8,6 +8,8 @@ export const checkUser =async(req,res) =>{
 
         const vaildpassword =await User.findOne({where:{password}})
         if(!vaildpassword) return res.status(400).json({message:"password not found"})
+
+
         res.status(200).json({message:"Login Succesfully"})
    }
    catch(err){
@@ -16,3 +18,79 @@ export const checkUser =async(req,res) =>{
    }
 }
 export default{checkUser}
+
+// import dotenv from "dotenv";
+// dotenv.config();
+// import jwt from "jsonwebtoken";
+// import User from "../Model/User.model.js";
+
+// export const checkUser = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     if (!process.env.MY_SECRET_KEY) {
+//       return res.status(500).json({ 
+//         message: "Secret key missing",
+//         token: null
+//       });
+//     }
+
+//     const user = await User.findOne({ where: { email } });
+//     if (!user) {
+//       // ❗ Token for invalid user
+//       const errorToken = jwt.sign(
+//         { invalid: true, reason: "email" },
+//         process.env.MY_SECRET_KEY,
+//         { expiresIn: "5m" }
+//       );
+//       return res.status(400).json({
+//         message: "Email not found",
+//         token: errorToken
+//       });
+//     }
+
+//     const validPassword = await User.findOne({ where: { password } });
+//     if (!validPassword) {
+//       // ❗ Token for invalid password
+//       const errorToken = jwt.sign(
+//         { invalid: true, reason: "password" },
+//         process.env.MY_SECRET_KEY,
+//         { expiresIn: "5m" }
+//       );
+//       return res.status(400).json({
+//         message: "Password not found",
+//         token: errorToken
+//       });
+//     }
+
+//     // ✅ Token for success login
+//     const token = jwt.sign(
+//       { id: user.id, email: user.email },
+//       process.env.MY_SECRET_KEY,
+//       { expiresIn: "1d" }
+//     );
+
+//     res.status(200).json({
+//       message: "Login Successfully",
+//       token,
+//     });
+
+//   } catch (err) {
+//     console.log(err);
+
+//     // ❗ Token for server error
+//     const errorToken = jwt.sign(
+//       { invalid: true, reason: "server_error" },
+//       process.env.MY_SECRET_KEY,
+//       { expiresIn: "5m" }
+//     );
+
+//     res.status(500).json({
+//       message: "Server Error",
+//       token: errorToken
+//     });
+//   }
+// };
+
+// export default { checkUser };
+
