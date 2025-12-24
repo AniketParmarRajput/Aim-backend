153b2278138252a44ff33effddb73cdb93588a05
AniketParmarRajput <rajputaniket092@gmail.com>
2025-12-24 17:00:59 +0530

    error in res solve it if you cannot the send the res to your req then in some limited time to your api braek it but chnage is done

--- src/Controllers/Prizing.controller.js
+++ src/Controllers/Prizing.controller.js
@@ -2,7 +2,7 @@ export const addPrizing = async (req,res) =>{
     console.log(req);
     try{
         const {itemName, amount, description, image } =req.body;
-        const jj=req.file.path && req.body;
+        const jj=req.body;
         console.log("Adding Prizing with data: ", req.body);
          console.log("Adding Prizing with data: ", req.file.path);
     return res.status(200).json({ success: true, message: "done", data:jj });
