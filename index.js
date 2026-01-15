5b63645f8fbc684a2b1e85c9ac79ac6929891f71
AniketParmarRajput <rajputaniket092@gmail.com>
2026-01-15 11:30:02 +0530

    backend index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ import isverify from "./src/MiddleWare/Auth.js";
 import db from "./src/Model/index.js";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000",  }));
 app.use(express.json());
 app.use(cookieParser());
 
