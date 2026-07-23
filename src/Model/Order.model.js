29a9d1ba632b7e7ffdd8a4d8ca9279a791b85959
AniketParmarRajput <rajputaniket092@gmail.com>
2026-07-23 16:10:01 +0530

    update order revcived show it

---
+++ src/Model/Order.model.js
@@ -0,0 +1,67 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../../Config/db.js";
+
+const Order = sequelize.define(
+  "Order",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    itemName: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    sku: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    paymentMethod: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: "cash on delivery",
+    },
+    status: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: "pending",
+    },
+    deliveryDate: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: "7 days",
+    },
+    cancelReason: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    address: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    mobile: {
+      type: DataTypes.STRING(20),
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "orders",
+    timestamps: false,
+  }
+);
+
+export default Order;
