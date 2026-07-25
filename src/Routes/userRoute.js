4b8e50ac8f2637fef8a0ba6bbcacf3c4f2b3416a
AniketParmarRajput <rajputaniket092@gmail.com>
2026-07-25 16:12:50 +0530

    backedn

--- src/Routes/userRoute.js
+++ src/Routes/userRoute.js
@@ -3,6 +3,7 @@ import {
   getEmployees,
   createEmployee,
   deleteEmployee,
+  getByEmail,
   getByEmployeeID,
   updatePassword
 } from "../Controllers/User.controller.js";
@@ -14,6 +15,7 @@ const router = express.Router();
 router.post("/",signupValidation, createEmployee);
 router.get("/get", getEmployees);
 router.delete("/delete/:id", deleteEmployee);
+router.get("/get-by-email/:email", getByEmail);
 router.get("/get/:id", getByEmployeeID)
 router.post("/update-password", updatePassword);
 export default router;
